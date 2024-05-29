8e6bfc973d764cd0673a5abfd88f44c2b35d9796
benasraudys <benas.raudys@gmail.com>
2024-05-30 00:01:14 +0300

    Implement a and b variables and refactor

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,14 +6,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Calculator extends JFrame implements ActionListener {
-    private JTextField display;
-    private double currentResult = 0;
+    private final JTextField display;
+
+    private double currentA = 0;
+    private double currentB = 0;
+    private int solveStatus = 0;
     private String currentOperator = "";
-    private Map<String, Operation> operations;
+    private final Map<String, Operation> operations;
 
     public Calculator() {
         setTitle("Calculator");
         setSize(540, 540);
+        ImageIcon icon = new ImageIcon("calc.png");
+        Image iconImage = icon.getImage();
+        setIconImage(iconImage);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
@@ -52,24 +58,59 @@ public class Calculator extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         String command = e.getActionCommand();
-
         if (command.equals("C")) {
-            currentResult = 0;
+            currentA = 0;
+            currentB = 0;
             currentOperator = "";
-            display.setText("");
+            solveStatus = 0;
+            display.setText("0");
         } else if (command.equals("=")) {
-            if (!currentOperator.isEmpty()) {
-                double secondOperand = Double.parseDouble(display.getText());
-                currentResult = operations.get(currentOperator).apply(currentResult, secondOperand);
-                display.setText(String.valueOf(currentResult));
-                currentOperator = "";
+            if (solveStatus >= 2) {
+                try {
+                    currentA = operations.get(currentOperator).apply(currentA, currentB);
+                    display.setText(String.valueOf(currentA));
+                    solveStatus = 3;
+                } catch (Exception ex) {
+                    display.setText("Error");
+                    solveStatus = 0;
+                    currentA = 0;
+                    currentB = 0;
+                    currentOperator = "";
+                }
             }
         } else if (operations.containsKey(command)) {
             currentOperator = command;
-            currentResult = Double.parseDouble(display.getText());
-            display.setText("");
+            if (solveStatus == 3) {
+                currentB = 0;
+            }
+            solveStatus = 2;
         } else {
-            display.setText(display.getText() + command);
+            if (solveStatus <= 1 && currentOperator.isEmpty()) {
+                currentA = addNewNumber(currentA, command);
+                display.setText("" + currentA);
+                solveStatus = 1;
+            } else if (!currentOperator.isEmpty() && solveStatus == 2) {
+                currentB = addNewNumber(currentB, command);
+                display.setText("" + currentB);
+                solveStatus = 2;
+            } else if (solveStatus == 3) {
+                currentA = Double.parseDouble(command);
+                currentB = 0;
+                currentOperator = "";
+                display.setText("" + currentA);
+                solveStatus = 1;
+            }
         }
+
+        System.out.println(currentA + " " + currentOperator + " " + currentB + " | " + solveStatus);
     }
+
+
+    public double addNewNumber(double currentNr, String newNumber) {
+        double parsedNewNumber = Double.parseDouble(newNumber);
+        currentNr *= 10;
+        currentNr += parsedNewNumber;
+        return currentNr;
+    }
+
 }
